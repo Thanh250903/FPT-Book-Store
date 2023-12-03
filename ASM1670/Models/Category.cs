5947ee1c349b721905d6a1672f3d4d4318e7ae6e
Phùng Khánh <khanhnoel2003@gmail.com>
2023-12-03 22:41:49 +0700

    Home Screen

---
+++ ASM1670/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace ASM1670.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [DisplayName("Display Order of Category")]
+        [Range(1, 100)]
+        public int DisplayOrder { get; set; }
+    }
+}
