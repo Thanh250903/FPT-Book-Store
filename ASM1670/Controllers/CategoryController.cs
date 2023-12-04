8e6831e63237e0d3acf2d3055a945b87772e4db4
Vu Tien Thanh <thanhtvgcd210498@fpt.edu.vn>
2023-12-04 23:32:29 +0700

    add controller

---
+++ ASM1670/Controllers/CategoryController.cs
@@ -0,0 +1,32 @@
+﻿using ASM1670.Data;
+using ASM1670.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASM1670.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBContext _dbContext;
+        public CategoryController(ApplicationDBContext applicationDBContext)
+        {
+            _dbContext = applicationDBContext;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categories = _dbContext.Categories.ToList();
+            return View(categories);
+        }
+        public IActionResult Details()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            _dbContext.Categories.Add(category);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+    }
+}
