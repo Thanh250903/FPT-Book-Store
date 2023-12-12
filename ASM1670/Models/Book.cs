71e1cda286884f028c0953026d07b52612cceaa3
Vu Tien Thanh <thanhtvgcd210498@fpt.edu.vn>
2023-12-12 23:34:14 +0700

    add some wibu image

--- ASM1670/Models/Book.cs
+++ ASM1670/Models/Book.cs
@@ -6,15 +6,15 @@ namespace ASM1670.Models
     public class Book
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string Author { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Author { get; set; }
         public double Price { get; set; }
         [ValidateNever]
         public int CategoryId { get; set; }
         [ForeignKey("CategoryId")]
         [ValidateNever]
-        public Category Category { get; set; }
+        public Category? Category { get; set; }
         [ValidateNever]
         public string? ImageUrl { get; set; }
     }
