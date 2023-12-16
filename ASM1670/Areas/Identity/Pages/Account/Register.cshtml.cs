e7065f3f8d3d5668a9e4b62dc2244bf1a51aefe7
Phùng Khánh <khanhnoel2003@gmail.com>
2023-12-16 13:58:38 +0700

    design page login signup

--- ASM1670/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ASM1670/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -62,6 +62,7 @@ namespace ASM1670.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+
         }
 
 
