9ec729c4e8b3ecc89728f9de21ba9994e196c44d
Phùng Khánh <khanhnoel2003@gmail.com>
2023-12-19 21:53:34 +0700

    Update View Models and edit Register form with new data in DBContext

--- ASM1670/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ASM1670/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
+using ASM1670.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
@@ -25,7 +26,6 @@ namespace ASM1670.Areas.Identity.Pages.Account
         private readonly RoleManager<IdentityRole> _roleManager;
         private readonly IUserEmailStore<IdentityUser> _emailStore;
         private readonly ILogger<RegisterModel> _logger;
-        private readonly IEmailSender _emailSender;
 
         public RegisterModel(
             UserManager<IdentityUser> userManager,
@@ -37,10 +37,10 @@ namespace ASM1670.Areas.Identity.Pages.Account
         {
             _userManager = userManager;
             _userStore = userStore;
-            _emailStore = GetEmailStore();
             _signInManager = signInManager;
+            //_emailStore = GetEmailStore();
             _logger = logger;
-            _emailSender = emailSender;
+            //_emailSender = emailSender;
             _roleManager = roleManager;
         }
         [BindProperty]
@@ -54,6 +54,9 @@ namespace ASM1670.Areas.Identity.Pages.Account
             [Display(Name = "Email")]
             public string Email { get; set; }
             [Required]
+            [Display(Name = "Full Name")]
+            public string FullName { get; set; }
+            [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
@@ -62,20 +65,24 @@ namespace ASM1670.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+            [Required]
+            [Display(Name = "Your phone number")]
+            public string PhoneNumber { get; set; }
+
+            // address and rolelist
+            [Required]
+            [Display(Name = "Your Address")]
+            public string HomeAddress { get; set; }
+            public IEnumerable<SelectListItem> SelectYourRole { get; set; }
+            [Required]
+            public string Role { get; set; }
 
         }
 
 
         public async Task OnGetAsync(string returnUrl = null)
         {
-            if (!_roleManager.RoleExistsAsync("User").GetAwaiter().GetResult())
-            {
-                _roleManager.CreateAsync(new IdentityRole("User")).GetAwaiter().GetResult();
-                _roleManager.CreateAsync(new IdentityRole("Admin")).GetAwaiter().GetResult();
-                _roleManager.CreateAsync(new IdentityRole("Owner")).GetAwaiter().GetResult();
-
-            }
-            Input = new InputModel();
+            GetRoles();
             ReturnUrl = returnUrl;
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
         }
@@ -86,28 +93,28 @@ namespace ASM1670.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = CreateUser();
-                await _userManager.AddToRoleAsync(user, "User");
-                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
-                await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                var user = new User(){
+                    UserName = Input.Email,
+                    Email = Input.Email,
+                    FullName = Input.FullName,
+                    EmailConfirmed = true,
+                    HomeAddress = Input.HomeAddress,
+                    PhoneNumber = Input.PhoneNumber,
+                };
                 var result = await _userManager.CreateAsync(user, Input.Password);
-
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
 
-                    var userId = await _userManager.GetUserIdAsync(user);
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmail",
-                        pageHandler: null,
-                        values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
-                        protocol: Request.Scheme);
+                    if (Input.Role == "User")
+                    {
+                        await _userManager.AddToRolesAsync(user, new[] { "User" });
+                    }
 
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
+                    if (Input.Role == "Owner")
+                    {
+                        await _userManager.AddToRolesAsync(user, new[] { "Owner" });
+                    }
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
                         return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
@@ -117,36 +124,50 @@ namespace ASM1670.Areas.Identity.Pages.Account
                         await _signInManager.SignInAsync(user, isPersistent: false);
                         return LocalRedirect(returnUrl);
                     }
+
                 }
                 foreach (var error in result.Errors)
                 {
                     ModelState.AddModelError(string.Empty, error.Description);
                 }
             }
+            GetRoles();
             return Page();
         }
 
-        private IdentityUser CreateUser()
-        {
-            try
-            {
-                return Activator.CreateInstance<IdentityUser>();
-            }
-            catch
-            {
-                throw new InvalidOperationException($"Can't create an instance of '{nameof(IdentityUser)}'. " +
-                    $"Ensure that '{nameof(IdentityUser)}' is not an abstract class and has a parameterless constructor, or alternatively " +
-                    $"override the register page in /Areas/Identity/Pages/Account/Register.cshtml");
-            }
-        }
+        //private IdentityUser CreateUser()
+        //{
+        //    try
+        //    {
+        //        return Activator.CreateInstance<IdentityUser>();
+        //    }
+        //    catch
+        //    {
+        //        throw new InvalidOperationException($"Can't create an instance of '{nameof(IdentityUser)}'. " +
+        //            $"Ensure that '{nameof(IdentityUser)}' is not an abstract class and has a parameterless constructor, or alternatively " +
+        //            $"override the register page in /Areas/Identity/Pages/Account/Register.cshtml");
+        //    }
+        //}
+        //private IUserEmailStore<IdentityUser> GetEmailStore()
+        //{
+        //    if (!_userManager.SupportsUserEmail)
+        //    {
+        //        throw new NotSupportedException("The default UI requires a user store with email support.");
+        //    }
+        //    return (IUserEmailStore<IdentityUser>)_userStore;
+        //}
 
-        private IUserEmailStore<IdentityUser> GetEmailStore()
+        private void GetRoles()
         {
-            if (!_userManager.SupportsUserEmail)
+            Input = new InputModel()
             {
-                throw new NotSupportedException("The default UI requires a user store with email support.");
-            }
-            return (IUserEmailStore<IdentityUser>)_userStore;
+                SelectYourRole = _roleManager.Roles.Where(x => x.Name != "Admin")
+                    .Select(x => x.Name).Select(x => new SelectListItem()
+                    {
+                        Text = x,
+                        Value = x
+                    })
+            };
         }
     }
 }
--- ASM1670/Models/ViewModels/BookVM.cs
+++ ASM1670/Models/ViewModels/BookVM.cs
@@ -6,7 +6,6 @@ namespace ASM1670.Models.ViewModels
     public class BookVM
     {
         public Book Book { get; set; }
-        [ValidateNever]
         public IEnumerable<SelectListItem> Categories { get; set; }
     }
 }
