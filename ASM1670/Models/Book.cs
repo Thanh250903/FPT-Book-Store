e5ebb85c6c4817a9cd646fe9d37c4919f925ccab
Phùng Khánh <khanhnoel2003@gmail.com>
2023-12-19 21:55:38 +0700

    add models

--- ASM1670/Models/Book.cs
+++ ASM1670/Models/Book.cs
@@ -1,21 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ASM1670.Models
 {
     public class Book
     {
+        [Key]
         public int Id { get; set; }
-        public string? Title { get; set; }
-        public string? Description { get; set; }
-        public string? Author { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0")]
+        [Required(ErrorMessage = "Price is required")]
         public double Price { get; set; }
-        [ValidateNever]
+        [Required]
+        public string Author { get; set; }
+
+        // [Required]
+        public string ImageUrl { get; set; }
+        [Required]
+        public int NuPages { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be greater than 0")]
+        [Required(ErrorMessage = "Quantity is required")]
+        public int Quantity { get; set; }
+        [Required]
         public int CategoryId { get; set; }
+
+        // link to category
         [ForeignKey("CategoryId")]
-        [ValidateNever]
-        public Category? Category { get; set; }
-        [ValidateNever]
-        public string? ImageUrl { get; set; }
+        public Category Category { get; set; }
+        public string CreateBy { get; set; }
     }
 }
