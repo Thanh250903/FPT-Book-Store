f61923396ab631dbfa3ac3786f8cf354eb50b1ce
Vu Tien Thanh <thanhtvgcd210498@fpt.edu.vn>
2023-12-19 22:22:01 +0700

    add view sua lai category

--- ASM1670/Areas/Customer/Controllers/HomeController.cs
+++ ASM1670/Areas/Customer/Controllers/HomeController.cs
@@ -2,7 +2,7 @@ using ASM1670.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
-namespace ASM1670.Areas.User.Controllers
+namespace ASM1670.Areas.Customer.Controllers
 {
     [Area("User")]
     public class HomeController : Controller
